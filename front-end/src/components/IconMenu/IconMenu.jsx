5f9f27c22581d2d7e9e4ddc220ed175b99144853
Konrad Bauer <konrad.bauer@remoterebels.com>
2026-02-21 15:03:57 +0100

    update iconCounter on cart icon

--- front-end/src/components/IconMenu/IconMenu.jsx
+++ front-end/src/components/IconMenu/IconMenu.jsx
@@ -3,9 +3,11 @@ import BAG_ICON from "../../assets/bag.svg";
 import HEART from "../../assets/heart.svg";
 
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/CartContext";
 
 export function IconMenu() {
-    const cartItems = 2;
+    const [cartItems] = useContext(CartContext);
 
     return (
         <ul className={styles.iconMenu}>
@@ -17,7 +19,7 @@ export function IconMenu() {
             <li>
                 <Link to="/koszyk">
                     <img src={BAG_ICON} />
-                    <div className={styles.numberOfProducts}>{cartItems}</div>
+                    <div className={styles.numberOfProducts}>{cartItems.length}</div>
                 </Link>
             </li>
         </ul>
